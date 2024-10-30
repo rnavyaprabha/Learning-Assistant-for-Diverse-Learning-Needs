f6d7b6d6941640e315ceea3457a62fc0385894d7
Martin <vivasdema@gmail.com>
2024-10-30 01:08:19 -0400

    Generating page to use the features

--- app/main.py
+++ app/main.py
@@ -7,15 +7,13 @@ import numpy as np
 import whisper 
 #from .WhisperSTT import transcribe_audio
 #from translator import translate_text
-#from .summarization import summarize_notes
+from .Summarization import summarize_notes
 import io
 
 app = FastAPI()
 
 # Load environment variables
 load_dotenv()
-print("Current Working Directory:", os.getcwd())
-print("API Key in main.py:", os.getenv("OPENAI_API_KEY"))  # Check if it loads correctly
 
 # Set the path for the HTML file
 html_file_path = os.path.join(os.path.dirname(__file__), "index.html")
@@ -31,17 +29,13 @@ async def websocket_endpoint(websocket: WebSocket):
         try:
             # Receive audio data from the client
             audio_data = await websocket.receive_bytes()
-
             # Process and transcribe audio data
             transcription = model.transcribe(np.frombuffer(audio_data, dtype=np.float32), fp16=False)
-
             # Send the transcription back to the client
             await websocket.send_text(transcription['text'])
-
         except Exception as e:
             await websocket.send_text(f"Error: {str(e)}")
             break
-
     await websocket.close()
 
 @app.get("/", response_class=HTMLResponse)
@@ -53,19 +47,21 @@ async def read_root():
 async def audio_to_text():#file: UploadFile = File(...)):
     #audio_data = await file.read()  # Read audio file from request
     #transcription = transcribe_audio(io.BytesIO(audio_data))
-    transcription = "My name is Martin and I am a software developer. Today we are going to learn how to use FastAPI."
+    # Transcription data from a local file
+    with open("app/notes.txt", "r") as file:
+        notes_content = file.read()
+    transcription=notes_content
     return {"transcription": transcription}
 
 # Endpoint for Translation
 @app.post("/translate/")
 async def translate():#text: str, target_language: str = "es"):
     ##translated_text = translate_text(text, target_language)
-    translated_text = 'Mi nombre es Martin y soy un desarrollador de software. El dia de hoy vamos a aprender a usar FastAPI.'
+    translated_text = 'Deep Learninges un subconjunto de la inteligencia artificial y el aprendizaje automático que aprovecha las redes neuronales para aprender de datos no estructurados, como imágenes, audio y texto. Utiliza estructuras de múltiples capas para comprender patrones y relaciones complejos dentro de los datos. Los avances clave incluyen redes neuronales convolucionales para el reconocimiento de imágenes y redes neuronales recurrentes para datos secuenciales. La retropropagación, las funciones de activación y las técnicas de regularización son fundamentales para el aprendizaje profundo. Los desafíos incluyen la necesidad de grandes conjuntos de datos, demandas computacionales y problemas de interpretabilidad. El aprendizaje profundo encuentra aplicaciones en la atención médica, el procesamiento del lenguaje natural, los sistemas autónomos, las finanzas y el entretenimiento. El futuro del aprendizaje profundo implica modelos livianos para dispositivos de borde, mejor interpretabilidad y aprendizaje multimodal para obtener información más completa.'
     return {"translation": translated_text}
 
 # Endpoint for Summarization
 @app.post("/summarize/")
-async def summarize():#text: str):
-    #summary = summarize_notes()
-    summary = 'We are going to learn hot to use FastAPI.'
+async def summarize():
+    summary = summarize_notes()
     return {"summary": summary}
